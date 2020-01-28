1595742a2c101ea72167b14c3aeaa4b0f91de3c4
KINGSMITH97 <44029916+KINGSMITH97@users.noreply.github.com>
2020-01-28 11:27:49 +0000

    Update Program.cs
    worked examples for

--- RoadToSuccess/RoadToSuccess/Program.cs
+++ RoadToSuccess/RoadToSuccess/Program.cs
@@ -10,7 +10,74 @@ namespace RoadToSuccess
     {
         static void Main(string[] args)
         {
-            //creating my first c# codes and my road to success 
+            //article 45 Question number one 
+
+            //   var count = 0;
+            //   var number = 0;
+            //   for (var i=1; i<100; i++)
+            //   {
+
+            //     if (i%3==0)
+            //     {
+            //         number = i;
+            //         count++;
+            //     }
+
+            //  }
+            //  Console.WriteLine(count);
+
+            //article 45 Question number two
+
+
+            //var SumOfNumbers = 0;
+            //while (true)
+            // {
+            //Console.WriteLine("Please enter a number or enter 'ok' to exit ");
+            //var UserInput = Console.ReadLine();
+            //if (UserInput == "ok")
+            // {
+            //Console.WriteLine("Program exited successfully");
+            // break;
+            // }
+            // SumOfNumbers += Convert.ToInt32(UserInput);
+            //}
+            //  Console.WriteLine("Sum of numbers is "+SumOfNumbers);
+
+            //article 45 Question number three 
+            // var factorial = 1;
+            //int number;
+            //Console.WriteLine("Please enter a number to be calculated");
+            //number = Convert.ToInt32(Console.ReadLine());
+            //for (int i=1; i<=number; i++)
+            //{
+            //factorial = factorial * i;
+            //}
+            //Console.WriteLine("factorial is " + factorial);
+
+
+
+            //article 45 Question number 4
+            //var random = new Random();
+            //var winningNumber = random.Next(1,10); 
+            //for (int i=1; i<5; i++)
+            //{
+            //Console.WriteLine("Please guess a number");
+            //var GuessNumber = Convert.ToInt32(Console.ReadLine());
+            //if (GuessNumber == winningNumber)
+            //{
+            //Console.WriteLine("Congratulations you won");
+            //break;
+            //}
+            //      else
+            //{
+            //Console.WriteLine("Wrong guess try again");
+            //}
+            // }
+
+
+            //
         }
+
+
     }
 }
