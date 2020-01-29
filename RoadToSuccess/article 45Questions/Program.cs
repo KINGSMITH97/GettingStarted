d8bc80901a3d41a6d8be8ef111b9abf85dcd5ac2
KINGSMITH97 <44029916+KINGSMITH97@users.noreply.github.com>
2020-01-29 02:33:13 +0000

    Create Program.cs

---
+++ RoadToSuccess/article 45Questions/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace article_45Questions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // article 45 question 1 
+            //Console.WriteLine("Please enter a number between 1 and 10");
+            //var userInput = Convert.ToInt32(Console.ReadLine());
+
+            // if (userInput >0 && userInput<11)
+            // {
+            //    Console.WriteLine("You've entered a valid number");
+            //} 
+            // else
+            //   Console.WriteLine("Number entered not valid");
+
+            //article 45 question 2
+            //   Console.WriteLine("Please enter the first number");
+            //   var FirstNum = Convert.ToInt32(Console.ReadLine());
+            //   var maxNum = FirstNum;
+
+            //    Console.WriteLine("Please enter the second number");
+            //    var SecondNum = Convert.ToInt32(Console.ReadLine());
+
+            //     if (SecondNum>FirstNum)
+            //   {
+            //       maxNum = SecondNum;
+            // }
+            // Console.WriteLine(string.Format("The maximum number among {0} and {1} is {2} ",FirstNum, SecondNum, maxNum ));
+
+
+            //article 45 question 3
+
+            // Console.WriteLine("Please enter the width of the picture in inches ");
+            // var width = Convert.ToInt32(Console.ReadLine());
+
+            // Console.WriteLine("Please enter the length of the picture in inches ");
+            // var Length = Convert.ToInt32(Console.ReadLine());
+
+            // if (width > Length)
+            // {
+            //     Console.WriteLine("The picture is landscape");
+            //  }
+            //  else
+            //      Console.WriteLine("the picture is portrait");
+
+            var numbers = new List<int>();
+               
+        }
+    }
+}
