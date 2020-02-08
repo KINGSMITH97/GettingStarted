7e99af87f3fab3c9c9788b20fa44af81a3c2a861
KINGSMITH97 <44029916+KINGSMITH97@users.noreply.github.com>
2020-02-08 11:13:37 +0000

    Create Program.cs

---
+++ RoadToSuccess/vowelcount/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vowelcount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a word");
+
+            var input = Console.ReadLine();
+            input.ToLower();
+
+            var vowels = new List<char>() { 'a', 'e', 'o', 'u', 'i' };
+            var CountVowels = 0;
+            foreach(var word in input)
+            {
+                if (vowels.Contains(word))
+                {
+                    CountVowels++;
+                }
+            }
+            Console.WriteLine("Number of vowels: " + CountVowels);
+        }
+    }
+}
