294ec6ecd10c8d57f7394d99cbd8166cdbaae73f
KINGSMITH97 <44029916+KINGSMITH97@users.noreply.github.com>
2020-02-08 10:56:46 +0000

    Create Program.cs

---
+++ RoadToSuccess/example4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace example4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter a sentence: ");
+            var input = Console.ReadLine();
+
+            if (String.IsNullOrWhiteSpace(input))
+            {
+                Console.WriteLine("Error");
+                return;
+            }
+
+            var variableName = "";
+            foreach (var word in input.Split(' '))
+            {
+                var wordWithPascalCase = char.ToUpper(word[0]) + word.ToLower().Substring(1);
+                variableName += wordWithPascalCase;
+            }
+
+            Console.WriteLine(variableName);
+        }
+
+}
+}
