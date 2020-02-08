4a4e9b44b0b3c0ab113de8cf252c735fe644a9fa
KINGSMITH97 <44029916+KINGSMITH97@users.noreply.github.com>
2020-02-08 10:19:53 +0000

    Create Program.cs

---
+++ RoadToSuccess/seprateNumbersByHyphen/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace seprateNumbersByHyphen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter a few numbers separted by hyphen eg:(1-2-3-4-5)");
+            var input = Console.ReadLine();
+
+            var numbers = new List<int>();
+            foreach (var number in input.Split('-'))
+            {
+                numbers.Add(Convert.ToInt32(number));
+            }
+            numbers.Sort();
+
+            var isConsecutive = true;
+            for (var i = 1; i < numbers.Count; i++)
+            {
+                if (numbers[i] != numbers[i - 1] + 1)
+                {
+                    isConsecutive = false;
+                    break;
+                }
+            }
+
+            var message = isConsecutive ? "Consecutive" : "Not Consecutive";
+            Console.WriteLine(message);
+
+        }
+       
+    }
+
+}
